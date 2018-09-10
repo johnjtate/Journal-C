29740633558e87922b0aebd884d2811a6b9b38c3
John Tate <johntate@Johns-MacBook-Pro.local>
2018-09-10 15:21:43 -0600

    Initial commit

---
+++ Journal-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by John Tate on 9/10/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Journal-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Journal-C
+//
+//  Created by John Tate on 9/10/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
